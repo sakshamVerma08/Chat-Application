9f0ab22f30de8995633e4d4e9aa8e5b6e26e484f
Saksham Verma <sakshamverma1000@gmail.com>
2025-04-28 19:02:40 +0530

    made messages component, and starting to write client side message sending logic

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -2,6 +2,19 @@ import React, { useState } from "react";
 
 const Messages = () => {
   const [message, setMessage] = useState("");
+
+  const handleMessages = (e) => {
+    e.preventDefault();
+
+    const message = e.target.firstElementChild.value;
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = ()=>{
+      console.log("Connected to the server");
+    }
+    socket.
+    setMessage("");
+  };
+
   return (
     <>
       <div className="w-screen h-screen  flex flex-col justify-between">
@@ -9,18 +22,23 @@ const Messages = () => {
 
         <div
           id="inputMessage"
-          className=" bg-green-500 h-1/4 w-full flex justify-around items-center border-2"
+          className="bg-amber-400 h-1/4 w-full flex justify-around items-center"
         >
-          <input
-            type="text"
-            className="bg-red-400 rounded-md py-10 h-3/4 
-             w-3/4 mx-5"
-            placeholder="Message..."
-            value={message}
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
-          />
+          <form
+            action="submit"
+            className="bg-red-400 h-3/4 w-2/3 rounded-xl"
+            onSubmit={handleMessages}
+          >
+            <input
+              type="text"
+              className="p-4 gap-2 h-12 w-full bg-gray-400 rounded-xl text-black"
+              placeholder="Message..."
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+            />
+          </form>
         </div>
       </div>
     </>
